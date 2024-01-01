f0818aeea98e0169240ad715e72b832f4e5056a0
Jassal24 <148603757+Jassal24@users.noreply.github.com>
2024-01-01 22:06:46 +0800

    Add files via upload

---
+++ ggwp/script.js
@@ -0,0 +1,5 @@
+
+  function showMessage() {
+    alert('Hello Tyne Happy New Year! Thank you for all the understandings every day and night, Youre so kind to me, even though Im so harsh to you. Youre sometimes angry, but still, you stay. I dont know how to give it back all the kindness that youve brought to me for the past few months. I dont know if it has been a month, but I assume na lang po haha. Btw, Im sorry because I always made you mad, and last time I heard you cry, sorry poo. Im thankful because were chatting every day and even calling. Have a great day and enjoy the year-end party po sa inyu!! ');
+}  
+
